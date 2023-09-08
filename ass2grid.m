cab2e15ed2ae01c12426a2201004745d71d332b9
dylankuming <126238923+dylankuming@users.noreply.github.com>
2023-09-08 17:24:05 +0200

    Add files via upload

--- ass2grid.m
+++ ass2grid.m
@@ -10,7 +10,7 @@ mic = [0, 0;      % Mic 1
        0, 0.5;    % Mic 3
        0.8, 0.5]; % Mic 4
 
-% Simulated source position (for example, [0.4, 0.3])
+% Simulated source position
 source_x = rand()*0.8;
 source_y = rand()*0.5;
 
@@ -41,9 +41,21 @@ for i = 2:4
     time_diffs(i-1) = lags(idx) / Fs;
 end
 
+% ... [previous code]
+
+% Generate symbolic hyperbolic equations
+syms x y
+
+hyperbolae = cell(1, 3);
+for i = 2:4
+    equation = sqrt((x - mic(i,1))^2 + (y - mic(i,2))^2) - sqrt(x^2 + y^2) + c * time_diffs(i-1);
+    hyperbolae{i-1} = equation;  % Ensuring it's an equation set to zero
+end
+
+
 % Generate a grid of points to evaluate the hyperbolic equation
-x_range = 0:0.01:0.8;
-y_range = 0:0.01:0.5;
+x_range = 0:0.001:0.8;
+y_range = 0:0.001:0.5;
 [X, Y] = meshgrid(x_range, y_range);
 
 % Plot the hyperbolas
@@ -54,41 +66,60 @@ for i = 2:4
     contour(X, Y, Z, [0 0], colors(i-1));
 end
 
-xlabel('X position (m)');
-ylabel('Y position (m)');
-title('TDoA Hyperbolas and Source Localization');
-grid on; axis equal;
 
-disp(['Actual source position: [', num2str(source_x), ',', num2str(source_y), ']']);
-disp(['Estimated source position: [', num2str(estimatedPos(1)), ',', num2str(estimatedPos(2)), ']']);
+solutions1 = solve([hyperbolae{1}, hyperbolae{2}], [x, y]);
+solutions2 = solve([hyperbolae{2}, hyperbolae{3}], [x, y]);
+solutions3 = solve([hyperbolae{1}, hyperbolae{3}], [x, y]);
 
-% ... [Previous code remains unchanged]
+% Extract x and y values of the intersections
+x_intersections1 = double(solutions1.x);
+y_intersections1 = double(solutions1.y);
+x_intersections2 = double(solutions2.x);
+y_intersections2 = double(solutions2.y);
+x_intersections3 = double(solutions3.x);
+y_intersections3 = double(solutions3.y);
 
-% Find intersections
-intersections = [];
-for i = 1:3
-    for j = i+1:4
-        % For each pair of mics (i, j)
-        Zi = c*time_diffs(i) + sqrt((X-mic(i+1,1)).^2 + (Y-mic(i+1,2)).^2) - sqrt((X-mic(1,1)).^2 + (Y-mic(1,2)).^2);
-        Zj = c*time_diffs(j-1) + sqrt((X-mic(j,1)).^2 + (Y-mic(j,2)).^2) - sqrt((X-mic(1,1)).^2 + (Y-mic(1,2)).^2);
-        c1 = contourc(x_range, y_range, Zi, [0 0]);
-        c2 = contourc(x_range, y_range, Zj, [0 0]);
-        intersec = InterX([c1(1,2:end);c1(2,2:end)],[c2(1,2:end);c2(2,2:end)]);
-        intersections = [intersections intersec];
-    end
-end
+% ... [Your code up to the point of extracting the intersections]
 
-% Calculate average of intersections as the estimated source position
-if ~isempty(intersections)
-    estimatedPos = [mean(intersections(1,:)), mean(intersections(2,:))];
-else
-    estimatedPos = [NaN, NaN];
-end
+% Getting the vertices of the triangle
+A = [x_intersections1, y_intersections1];
+B = [x_intersections2, y_intersections2];
+C = [x_intersections3, y_intersections3];
 
-% ... [Display and plotting remains unchanged]
+% Compute centroid
+centroid_x = (A(1) + B(1) + C(1)) / 3;
+centroid_y = (A(2) + B(2) + C(2)) / 3;
+
+centroid = [centroid_x, centroid_y];
+source = [source_x, source_y];
+
+% Display the centroid
+disp('Centroid of the triangle:');
+disp(centroid);
+
+% Display the source
+disp('Source:');
+disp(source);
+
+% Plot the intersections (vertices of the triangle)
+%plot([A(1), B(1), C(1)], [A(2), B(2), C(2)], 'ko', 'MarkerSize', 10, 'DisplayName', 'Intersections');
+
+% Plot the microphones' positions
+plot(mic(:,1), mic(:,2), 'k^', 'MarkerSize', 12, 'DisplayName', 'Microphones');
 
 
-disp(['Estimated source position: [', num2str(estimatedPos(1)), ',', num2str(estimatedPos(2)), ']']);
+% Plot the source position
+plot(source_x, source_y, 'mx', 'MarkerSize', 10, 'DisplayName', 'Source');
 
+% Plot the centroid of the triangle
+plot(centroid_x, centroid_y, 'o', 'Color', [0.6 0.2 0], 'MarkerSize', 10, 'DisplayName', 'Centroid');
+
+% Some graphical adjustments
+legend('show'); % To show a legend on the plot
+xlabel('X Coordinate');
+ylabel('Y Coordinate');
+title('Hyperbolae Intersections, Source, and Centroid');
+grid on;
+axis([0 0.8 0 0.5]); % Setting the axis limits
 
 
